7dda72519a41c326797d2b161ccbe25087d0eae0
Elvis Mihasi <visimihasi0@gmail.com>
2025-02-13 11:31:40 +0100

    Initial Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+/**
+ * @author Lufthansa Industry Solutions
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
